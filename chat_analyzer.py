d6daabfa4f4e4974c08d30f175272ef1feb42f72
amangang3 <aman.ganglani.16@ucl.ac.uk>
2020-04-06 17:17:51 +0400

    converted byte objects to string objects

--- chat_analyzer.py
+++ chat_analyzer.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 
-
 def read_dates(filename):
     with open(filename,'rb') as fp:
         soup = BeautifulSoup(fp,'lxml')
@@ -8,6 +7,7 @@ def read_dates(filename):
         dates = []
         for element in dates_finder:
             element = element.text.strip().encode('utf-8')
+            element = element.decode('utf-8').splitlines()
             print(element)
             dates.append(element)
 read_dates("message_18.html")
